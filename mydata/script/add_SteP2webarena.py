0604cb3344abe9a0e607c304bbc10419b806cb58
lgy0404 <2528379691@qq.com>
2024-07-19 15:40:25 +0800

    迁移完成step所有结果

--- mydata/script/add_SteP2webarena.py
+++ mydata/script/add_SteP2webarena.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # 读取两个Excel文件
-df1 = pd.read_excel('SteP_WebArena.xlsx')
-df2 = pd.read_csv('log/map/summary.csv')
+df1 = pd.read_excel('mydata/process/SteP_WebArena.xlsx')
+df2 = pd.read_csv('log-all/summary.csv')
 
 # 合并数据
 merged_df = pd.merge(df1, df2, on='task_id', how='outer')
 
 # 保存合并后的数据到新的Excel文件
-merged_df.to_excel('merged_data.xlsx', index=False)
+merged_df.to_excel('mydata/SteP_WebArena_all_result.xlsx', index=False)
